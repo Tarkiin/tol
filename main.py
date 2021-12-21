578e9acae9e92da994f01a125cfdb9339939506a
xevaly <xev.github@liqht.me>
2021-12-21 14:31:45 +0100

    Finish

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
+import json
+import sys
+import threading
 import time
-import discord
-
+import websocket
+from concurrent.futures import ThreadPoolExecutor
 
 def updateTokens():
     with open('tokens.txt', 'r') as f:
         return f.read().splitlines()
 
 
-class Onliner:
-    def __init__(self, token):
-        bot = discord.Client(self_bot=True)
-        bot.run(token, bot=False)
+def onliner(token):
+    w = websocket.WebSocket()
+    w.connect('wss://gateway.discord.gg/?v=6&encoding=json')
+    jsonObj = json.loads(w.recv())
+    interval = jsonObj['d']['heartbeat_interval']
+    w.send(json.dumps({
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": sys.platform,
+                "$browser": "RTB",
+                "$device": f"{sys.platform} Device"
+            },
+            "presence": {
+                "game": {
+                    "name": 'Token Onliner - TOL',
+                    "type": 0,
+                    "details": "Made by xevaly @ https://die.ooo",
+                    "state": "https://www.github.com/xevaly/tol"
+                },
+                "status": '>> die.ooo',
+                "since": 0,
+                "afk": False
+            }
+        },
+        "s": None,
+        "t": None
+    }))
+    while True:
+        time.sleep(interval / 1000)
+        w.send(json.dumps({"op": 1, "d": None}))
 
 
 def main():
-    oldTokens = []
-    onlineTokens = []
+    executor = ThreadPoolExecutor(max_workers=100)
     while True:
         tokens = updateTokens()
         for token in tokens:
-            if not (token in oldTokens):
-                onlineTokens.append(Onliner(token))
-        time.sleep(300)
-        oldTokens = tokens
+            print(f'Starting {token}')
+            threading.Thread(target=onliner, args=(token, )).start()
+        time.sleep(540)
 
 
 if __name__ == '__main__':
