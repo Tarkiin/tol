a2c6e64d0b4816841c5976c224dcaed18e83ee1c
Tarkiin <61888270+Tarkiin@users.noreply.github.com>
2022-01-09 23:35:03 +0100

    Random games added main.py
    Random games added
    
    sorry if i'm not correct, i don't know about programming i'm just using common sense

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 import threading
 import time
 import websocket
+import random
+from concurrent.futures import ThreadPoolExecutor
+
+games = ['Minecraft', 'Rust', 'VRChat', 'reeeee', 'MORDHAU', 'Fortnite', 'Apex Legends', 'Escape from Tarkov', 'Rainbow Six Siege', 'Counter-Strike: Global Offense', 'Sinner: Sacrifice for Redemption', 'Minion Masters', 'King of the Hat', 'Bad North', 'Moonlighter', 'Frostpunk', 'Starbound', 'Masters of Anima', 'Celeste', 'Dead Cells', 'CrossCode', 'Omensight', 'Into the Breach', 'Battle Chasers: Nightwar', 'Red Faction Guerrilla Re-Mars-tered Edition', 'Spellforce 3', 'This is the Police 2', 'Hollow Knight', 'Subnautica', 'The Banner Saga 3', 'Pillars of Eternity II: Deadfire', 'This War of Mine', 'Last Day of June', 'Ticket to Ride', 'RollerCoaster Tycoon 2: Triple Thrill Pack', '140', 'Shadow Tactics: Blades of the Shogun', 'Pony Island', 'Lost Horizon', 'Metro: Last Light Redux', 'Unleash', 'Guacamelee! Super Turbo Championship Edition', 'Brutal Legend', 'Psychonauts', 'The End Is Nigh', 'Seasons After Fall', 'SOMA', 'Trine 2: Complete Story', 'Trine 3: The Artifacts of Power', 'Trine Enchanted Edition', 'Slime-San', 'The Inner World', 'Bridge Constructor', 'Bridge Constructor Medieval', 'Dead Age', 'Risk of Rain', "Wasteland 2: Director's Cut", 'The Metronomicon: Slay The Dance Floor', 'TowerFall Ascension + Expansion', 'Nidhogg', 'System Shock: Enhanced Edition', 'System Shock 2', "Oddworld:New 'n' Tasty!", 'Out of the Park Baseball 18', 'Hob', 'Destiny 2', 'Torchlight', 'Torchlight 2', 'INSIDE', 'LIMBO', "Monaco: What's Yours Is Mine", 'Tooth and Tail', 'Dandara', 'GoNNER', 'Kathy Rain', 'Kingdom: Classic', 'Kingdom: New Lands', 'Tormentor X Punisher', 'Chaos Reborn', 'Ashes of the Singularity: Escalation', 'Galactic Civilizations III', 'Super Meat Boy', 'Super Hexagon', 'de Blob 2', 'Darksiders II Deathinitive Edition', 'Darksiders Warmastered Edition', 'de Blob', 'Red Faction 1', 'Dungeon Defenders']
 
 def updateTokens():
     with open('tokens.txt', 'r') as f:
@@ -25,12 +29,12 @@ def onliner(token):
             },
             "presence": {
                 "game": {
-                    "name": 'Token Onliner - TOL',
+                    "name": (random.choice(games)),
                     "type": 0,
-                    "details": "Made by xevaly @ https://die.ooo",
-                    "state": "https://www.github.com/xevaly/tol"
+                    "details": "Line 2",
+                    "state": "Line 3"
                 },
-                "status": '>> die.ooo',
+                "status": '>> Online',
                 "since": 0,
                 "afk": False
             }
@@ -44,14 +48,12 @@ def onliner(token):
 
 
 def main():
-    oldTokens = []
+    executor = ThreadPoolExecutor(max_workers=100)
     while True:
         tokens = updateTokens()
         for token in tokens:
-            if not(token in oldTokens):
-                print(f'Starting {token}')
-                threading.Thread(target=onliner, args=(token, )).start()
-                oldTokens.append(token)
+            print(f'Starting {token}')
+            threading.Thread(target=onliner, args=(token, )).start()
         time.sleep(540)
 
 
